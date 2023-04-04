5ad00923fc633bac8a140c00fbe2c74f2121a972
Yoaquin <75709007+YoAquinJs@users.noreply.github.com>
2023-04-04 12:31:20 -0500

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 sourceDir=$(pwd)
-destinationZip="$sourceDir/pacman.love"
-excludeExtensions=("*.love" "*.gitignore" "*.ps1" "*.md" "*.sh" "*.zip")
+destinationZip="$sourceDir/compressed.zip"
+excludeExtensions=("love" "gitignore" "ps1" "md" "sh" "zip")
 excludeFolders=(".vscode" ".git")
 
 if [ -f "$destinationZip" ]; then
@@ -12,8 +12,8 @@ fi
 filesToArchive=()
 while IFS= read -r -d $'\0' file; do
     exclude=0
-    for excludeExtension in ${excludeExtensions[@]}; do
-        if [[ "$file" == *"$excludeExtension" ]]; then
+    for excludeExtension in "${excludeExtensions[@]}"; do
+        if [[ "$file" == *".$excludeExtension" ]]; then
             exclude=1
             break
         fi
@@ -23,8 +23,8 @@ while IFS= read -r -d $'\0' file; do
         continue
     fi
 
-    for excludeFolder in ${excludeFolders[@]}; do
-        if [[ "$file" == *"$excludeFolder"* ]]; then
+    for excludeFolder in "${excludeFolders[@]}"; do
+        if [[ "$file" == *"/$excludeFolder/"* || "$file" == *"/$excludeFolder" ]]; then
             exclude=1
             break
         fi
@@ -37,18 +37,5 @@ while IFS= read -r -d $'\0' file; do
     filesToArchive+=("$file")
 done < <(find "$sourceDir" -type f -print0)
 
-mkdir "$sourceDir/files"
+zip -r "$destinationZip" "${filesToArchive[@]/$sourceDir\//}"
 
-for file in "${filesToArchive[@]}"; do
-    relativePath=$(echo "$file" | sed "s|^$sourceDir||" | sed 's|^/||')
-    zipEntry="files/$relativePath"
-    zipEntryDir=$(dirname "$zipEntry")
-    if [ ! -d "$zipEntryDir" ]; then
-        mkdir -p "$zipEntryDir"
-    fi
-    cp "$file" "$zipEntry"
-done
-
-zip -r "$destinationZip" "$sourceDir/files"
-
-rm -r "$sourceDir/files"
